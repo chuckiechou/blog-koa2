43757cf4443982d85eac28255ecec21d80053aa3
chuckiechou <zzp208@qq.com>
2019-07-20 08:25:52 +0800

    update

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session');
-const redisStore = require('koa-redis')
+const redisStore = require('koa-redis');
+const path = require('path');
+const fs = require('fs');
+const morgan = require('koa-morgan');
 
 const { REDIS_CONF } = require('./conf/db')
 
@@ -38,7 +41,20 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
-app.keys = ['Usy784934nasfa***1212_']
+const ENV = process.env.NODE_ENV;
+if (ENV !== 'prod') {
+  app.use(morgan('dev'));
+} else {
+  const logFileName = path.join(__dirname, 'logs', 'access.log');
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(morgan('combined', {
+    stream: writeStream
+  }));
+}
+
+app.keys = ['888888888877373270-shyaujsnx']
 app.use(session({
   cookie: {
     path: '/',
